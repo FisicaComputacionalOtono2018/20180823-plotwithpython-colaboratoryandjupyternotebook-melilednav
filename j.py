e3fc4be6c9d897b81f698142466f4951877f996d
Melissa Ledesma <42181845+melilednav@users.noreply.github.com>
2018-08-23 11:59:01 -0500

    Add files via upload

---
+++ j.py
@@ -0,0 +1,25 @@
+s=12
+p=input("inserrte un valor ")
+
+def asfg(x):
+ if x<2:
+  flag=False
+ elif x==2:
+  flag=True
+ else:
+  flag=True
+  for i in range (2,x-1)=
+   if x%i==0:
+    flag=False
+    break
+ return flag
+
+while s!=0:
+ p=p+2
+  while p<5 
+  s=s-p
+  p=p+2
+
+
+  
+
